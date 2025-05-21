eaadc149904922abe893223b696b96b69b5d7086
Sachila2003 <sachilageeth1@gmail.com>
2025-05-21 05:31:39 +0530

    event listner complete

--- js/app.js
+++ js/app.js
@@ -13,9 +13,51 @@ document.addEventListener('DOMContentLoaded', function () {
     const createStoryBtn = document.getElementById('create-story');
     const newStroryTitleInput = document.getElementById('new-story-title');
     const newStoryFirstParagraphInput = document.getElementById('new-story-first-paragraph');
+    const imageUpload = document.getElementById('story-image-upload');
+    const imagePreview = document.getElementById('preview-image');
+    const storyImageContainer = document.getElementById('story-image-container');
 
     //state
     let currentStoryId = null;
+    let selectImageUrl = null;
+
+    // image upload handler
+    imageUpload.addEventListener('change', function (e) {
+        const file = e.target.files[0];
+        if (file) {
+            //validate image size 5mb
+            if (file.size > 1024 * 1024 * 5) {
+                alert('Image size cannot be greater than 5MB');
+                return;
+            }
+            // show loading state
+            imagePreview.classList.add('loading');
+
+            const reader = new FileReader();
+            reader.onload = function (event) {
+                selectImageUrl = event.target.result;
+                previewImg.src = selectImageUrl;
+
+                //ensure image is loading befor show 
+                previewImg.onload = function () {
+                    imagePreview.classList.remove('loading');
+                    imagePreview.style.display = 'block';
+
+                    if (this.naturalHeight > this.naturalWidth) {
+                        this.style.ObjectPositon = 'center top';
+                    } else {
+                        this.style.ObjectPositon = 'center center'
+                    }
+                };
+
+                previewImg.onerror = function () {
+                    imagePreview.classList.remove('loading');
+                    alert("Error loading image, try again");
+                };
+            };
+            reader.readAsDataURL(file);
+        }
+    })
 
     function loadStories() {
         // load all stories from firebase
@@ -25,19 +67,25 @@ document.addEventListener('DOMContentLoaded', function () {
 
             Object.entries(stories).forEach(([id, story]) => {
                 const lastParagraph = story.paragraphs
-                ? Object.values(story.paragraphs)[Object.values(story.paragraphs).length - 1]
-                : {text: 'No paragraph yet'};
+                    ? Object.values(story.paragraphs)[Object.values(story.paragraphs).length - 1]
+                    : { text: 'No paragraph yet' };
 
                 const storyCard = document.createElement('div');
                 storyCard.className = 'story-card';
                 storyCard.innerHTML = `
+                ${story.coverImage ?
+                        `<div class="story-card-image-container">
+                    <img src="${story.coverImage}"
+                    class="story-card-image" alt="${story.title}">
+                    </div>`: ``}
+                }
                     <div class="story-card-content">
                         <h3>${story.title}</h3>
                         <p>${lastParagraph.text}</p>
                     </div>
                     <div class="story-card-footer">
                         <span>${story.paragraphs ? Object.keys
-                            (story.paragraphs).length : 0} Paragraphs &nbsp;</span>
+                        (story.paragraphs).length : 0} Paragraphs &nbsp;</span>
                         <button class="like-btn" data-story-id="${id}">
                             <i class="far fa-heart"></i>
                         </button>
@@ -55,8 +103,22 @@ document.addEventListener('DOMContentLoaded', function () {
         currentStoryId = id;
         storyTitle.textContent = story.title;
         storyContent.innerHTML = '';
+        if (story.coverImage) {
+            storyImageContainer.innerHTML = `
+                 <img class="${story.coverImage}" class="story-image" alt=${story.title}">`;
+                 storyImageContainer.style.display.querySelector('img');
+                 imagePreview.onload = function (){
+                    if(this.naturalHeight > this.naturalWidth){
+                        this.style.ObjectPositon = 'center top';
+                    }else{
+                        this.style.ObjectPositon = 'center center';
+                    }
+                 };
+        } else {
+            storyImageContainer.style.display = 'none';
+        }
 
-        if(story.paragraphs){
+        if (story.paragraphs) {
             Object.entries(story.paragraphs).forEach(([paragraphId, paragraph]) => {
                 const paragraphE1 = document.createElement('div');
                 paragraphE1.className = 'paragraph';
@@ -81,7 +143,8 @@ document.addEventListener('DOMContentLoaded', function () {
         document.querySelectorAll('paragraph-like-btn').forEach(btn => {
             btn.addEventListener('click', (e) => {
                 e.stopPropagation();
-                
+                likeParagraph(btn.CDATA_SECTION_NODE.storyId, btn.CDATA_SECTION_NODE.paragraphId);
+
             });
         });
     }
@@ -98,9 +161,15 @@ document.addEventListener('DOMContentLoaded', function () {
             return;
         }
 
+        if(!selectImageUrl){
+            alert('Please select a cover image');
+            return;
+        }
+
         const newStoryref = database.ref('stories').push();
         newStoryref.set({
             title: title,
+            coverImage: selectImageUrl,
             createAt: firebase.database.ServerValue.TIMESTAMP,
             paragraphs: {
                 first: {
@@ -113,7 +182,42 @@ document.addEventListener('DOMContentLoaded', function () {
             newStoryModal.style.display = 'none';
             newStroryTitleInput.value = '';
             newStoryFirstParagraphInput.value = '';
+            selectImageUrl = null;
+            imagePreview.style.display = 'none';
+
+            viewStory(newStoryref.id,{
+                title:title,
+                coverImage: selectImageUrl,
+                paragraph:{
+                    first:{
+                        text: firstParagraph,
+                        likes:0
+                    }
+                }
+            });
         });
+    }
+    //like a story
+    function likeStory(storyId){
+        const paragraphref = database.ref(`stories/${storyId}/paragraphs/${paragraphId}/likes`);
+        paragraphref.transaction((currentLikes) =>{
+            return (currentLikes || 0) +1;
+        })
+    }
+    //add a new paragraph
+    function addParagraph(storyId, text){
+        if(text.length < 50){
+            alert('Paragraph must be at least 50 characters long');
+            return;
+        }
+        const newParagraphrefs = database.ref(`stories/${storyId}/
+            paragraphs`).push();
+            newParagraphrefs.set({
+                text: text,
+                timestamp: firebase.database.ServerValue.TIMESTAMP,
+                likes: 0
+            });
+            newParagraphInput.value = '';
     }
 
     //Event Listeners
@@ -127,6 +231,17 @@ document.addEventListener('DOMContentLoaded', function () {
 
     closeModalBtn.addEventListener('click', () => {
         newStoryModal.style.display = 'none';
+    });
+    backButton.addEventListener('click', () =>{
+        newStoryModal.style.display = 'none';
+        document.querySelector('.stories-container').style.display = 'block';
+        currentStoryId = null;
+    });
+
+    submitParagraphBtn.addEventListener('click', () => {
+       if(currentStoryId){
+          addParagraph(currentStoryId, newParagraphInput.value)
+       }
     });
 
     window.addEventListener('click', (e) => {
